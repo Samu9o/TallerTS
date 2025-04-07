ac10dc42010393e0604c94a861c65f512440fd83
UsuarioGithub <oscivanov@gmail.com>
2025-04-06 21:41:07 -0500

    Commit Taller 1

---
+++ src/data.ts
@@ -0,0 +1,9 @@
+import { Serie } from "./series.js";
+
+export const series: Serie[] = [
+  new Serie(1, "Breaking Bad", "AMC", 5),
+  new Serie(2, "The Crown", "Netflix", 4),
+  new Serie(3, "Fargo", "FX", 3),
+  new Serie(4, "Westworld", "HBO", 3),
+  new Serie(5, "The Mandalorian", "Disney+", 2)
+];
